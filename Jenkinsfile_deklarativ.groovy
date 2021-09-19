a70acb392dd53b05d18134601136291696adeaf1
Demo User Jenkins Pipeline Workshop <tn00@comquent.academy>
2021-09-19 15:35:33 +0200

    Update Jenkinsfile_deklarativ.groovy

--- Jenkinsfile_deklarativ.groovy
+++ Jenkinsfile_deklarativ.groovy
@@ -1,35 +1,31 @@
 pipeline() {
     agent any
-    environment {
-        mvnVersion = "MVN36"
-        javaVersion = "JAVA8"
+    tools {
+        maven 'MVN360' 
+        java  'JDK8' 
     }
     stages {
         stage('prepare') {
             steps {
                 deleteDir()
-                withMaven(jdk: 'JDK8', maven: 'MVN360', publisherStrategy: 'EXPLICIT') {
-                    sh "mvn -version"
-                }
+                sh "mvn -version"
             }
         }
 
         stage('build') {
             steps {
-                withMaven(jdk: 'JDK8', maven: 'MVN360', publisherStrategy: 'EXPLICIT') {
-                    sh "mvn install -Dmaven.test.skip=true"
-                }
+                sh "mvn install -Dmaven.test.skip=true"
                 archiveArtifacts artifacts: 'target/**/*.jar'
             }
         }
 
         stage('test') {
             steps {
-                withMaven(jdk: 'JDK8', maven: 'MVN360', publisherStrategy: 'EXPLICIT') {
+                script {
                     def retSt = sh returnStatus: true, script: 'mvn test'
                     echo "Beendet mit " + retSt 
+                    junit 'target/surefire-reports/*Tests.xml'
                 }
-                junit 'target/surefire-reports/*Tests.xml'
             }
         }
     }
