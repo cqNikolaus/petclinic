6c680793f7cccae5ba4b2199a434b8926b0b3f21
Demo User Jenkins Pipeline Workshop <tn00@comquent.academy>
2022-09-19 15:41:42 +0000

    Update Jenkinsfile_K8s_Build.groovy

--- Jenkinsfile_K8s_Build.groovy
+++ Jenkinsfile_K8s_Build.groovy
@@ -10,7 +10,7 @@ pipeline {
   stages {
     stage('Build') {
       steps {  // no container directive is needed as the maven container is the default
-        sh "mvn clean package"   
+        sh "mvn package -Dmaven.test.skip=true"   
       }
     }
     stage('Build Docker Image') {
