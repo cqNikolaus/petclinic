f314d3849d5ecb347ab07fcbe937860278f20e49
Clemens Nikolaus <clemens.nikolaus@comquent.de>
2025-01-09 11:19:00 +0100

    Update test

--- src/test/java/org/springframework/samples/petclinic/owner/OwnerControllerTests.java
+++ src/test/java/org/springframework/samples/petclinic/owner/OwnerControllerTests.java
@@ -1,1 +1,31 @@
+package org.springframework.samples.petclinic.owner;
 
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+@RunWith(SpringRunner.class)
+@WebMvcTest(OwnerController.class)
+public class OwnerControllerTests {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Mock
+    private OwnerRepository ownerRepository;
+
+    @Before
+    public void setup() {
+        MockitoAnnotations.initMocks(this);
+    }
+
+    @Test
+    public void dummyTest() {
+    }
+}
